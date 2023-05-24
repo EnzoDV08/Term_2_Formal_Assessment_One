1c3d16539d84339c2f30b406586515dec7a18596
EnzoDV08 <125486388+EnzoDV08@users.noreply.github.com>
2023-05-24 10:55:04 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function validateForm() {
+    var checkbox = document.getElementById("checkbox");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var contact = document.getElementById("contact").value;
+    var subject = document.getElementById("subject").value;
+  
+    if (checkbox.checked) {
+      alert(
+        "Welcome " +
+          name +
+          "! Thank you for your interest in " +
+          subject +
+          "! We will contact you via the email provided: " +
+          email +
+          " or alternatively on the phone number: " +
+          contact
+      );
+    } else {
+      alert(
+        "Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested " +
+          name +
+          "! If you want to sign up, you better CHECK that newsletter box!"
+      );
+    }
+
+    console.log(validateForm)
+  }
